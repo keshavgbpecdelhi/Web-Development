75194dc4d6189630d7b5e9a0b263a3d5802cddfd
Pranit Utekar <89144527+Pranit-02@users.noreply.github.com>
2022-10-08 17:54:01 +0530

    Added New Project Math4Kids

---
+++ Math4Kids/scripts/multiply.js
@@ -0,0 +1,61 @@
+const option1 = document.getElementById("option1"),
+      option2 = document.getElementById("option2"),
+      option3 = document.getElementById("option3"),
+      audio = document.getElementById("myAudio");  
+var answer = 0;
+
+function generate_equation(){ 
+  var num1 = Math.floor(Math.random() * 13),
+      num2 = Math.floor(Math.random() * 13),
+      dummyAnswer1 = Math.floor(Math.random() * 50),
+      dummyAnswer2 = Math.floor(Math.random() * 50),
+      allAnswers = [],
+      switchAnswers = [];
+
+  answer = eval(num1 * num2);
+  
+  document.getElementById("num1").innerHTML = num1; 
+  document.getElementById("num2").innerHTML = num2; 
+
+  allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+  for (i = allAnswers.length; i--;){
+    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+  };
+
+  option1.innerHTML = switchAnswers[0];
+  option2.innerHTML = switchAnswers[1];
+  option3.innerHTML = switchAnswers[2]; 
+
+};
+
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){
+      generate_equation();
+    }
+    else{
+      audio.play();
+    }
+});
+
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+      generate_equation();
+    }
+    else{
+      audio.play();
+    }
+});
+
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+     generate_equation();
+    }
+    else{
+      audio.play();
+    }
+});
+
+generate_equation()
+
+
