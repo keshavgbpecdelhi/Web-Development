353a7405a054771f47fe768e1e3eceea5ebe5c6c
HARSH-VATS-6 <73696588+HARSH-VATS-6@users.noreply.github.com>
2022-10-08 14:23:22 +0530

    Create OrdinalNumber.java
    Hacktoberfest event

---
+++ OrdinalNumber.java
@@ -0,0 +1,29 @@
+// A Java program for generating the first 20 ordinal numbers.  
+public class OrdinalNumber  
+{  
+// main method  
+public static void main(String argvs[])  
+{  
+// loop to iterate till 20 to generate  
+// the first 20 ordinal numbers  
+for(int i = 1; i <= 20; i++)  
+{  
+if(i == 1)  
+{  
+System.out.println("The " + i + "st ordinal number.");  
+}  
+else if(i == 2)  
+{  
+System.out.println("The " + i + "nd ordinal number.");  
+}  
+else if(i == 3)  
+{  
+System.out.println("The " + i + "rd ordinal number.");  
+}  
+else  
+{  
+System.out.println("The " + i + "th ordinal number.");  
+}  
+}  
+}  
+}  
