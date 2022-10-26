e83ed93c5f92e2381c5fd99f4c4f04fe2e240a60
Saksham <97455464+saxam0@users.noreply.github.com>
2022-10-26 22:53:24 +0530

    added clone files

---
+++ Myntra-Clone-main/utils.css
@@ -0,0 +1,36 @@
+.flex{
+    display:flex;
+  }
+  
+  .items-center{
+    align-items:center;
+  } 
+  
+  .justify-center{
+    justify-content: center;
+  }
+  
+  .space-between{
+    justify-content: space-between;
+  }
+  
+  .uppercase{
+    text-transform: uppercase;
+  }
+  
+  .semibold{
+    font-weight: bolder;
+  }
+  
+  .mx-2{
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+  
+  .my-2{
+    margin: 12px 0;
+  }
+  
+  .flex-wrap{
+    flex-wrap: wrap;
+  }
