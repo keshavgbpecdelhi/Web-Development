0035e1b81bf0002dd1beb0bc7efaa21a604a9ce6
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-07-04 19:43:43 +0530

    commented gayab

--- 2048 Game/app.js
+++ 2048 Game/app.js
@@ -4,12 +4,6 @@ const board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
-    // const board = [
-    //     [0, 2, 4, 8],
-    //     [16, 32, 64, 128],
-    //     [256, 512, 1024, 0],
-    //     [0, 0, 0, 0]
-    // ]
 const boardContainer = document.querySelector(".board");
 document.addEventListener('swiped-up', function(e) {
     console.log(e.target); // the element that was swiped
