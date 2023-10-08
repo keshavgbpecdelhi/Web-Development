a408e7792edb56829dd583d73bb4043089ae0a94
Pushkaran <pushkaranjps@gmail.com>
2023-10-08 11:32:28 +0530

    first commit

---
+++ Non-Profit-Organization/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
+}
+
+
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50 ? header.classList.add("active")
+    : header.classList.remove("active");
+});
--- README.md
+++ README.md
@@ -76,6 +76,8 @@ Web Development Demo Pages collection<br><br>
 | Review Cards | [Code](https://github.com/31Sanskrati/keshavgbpecdelhi/tree/master/Review%20Cards) | [Demo](https://codepen.io/Sanskrati01/pen/yLbxboO) |
 | Sorting Visualizer | [Code](https://github.com/keshavgbpecdelhi/Web-Development/tree/master/Sorting_Visualizer) | [Demo](https://sortingvisualizera.netlify.app/) |
 | Age Calculator | [Code](https://github.com/DevanshMudgal/Web-Development/tree/master/AgeCalculator) | [Demo](https://demo.100jsprojects.com/age-calculator) |
+| Non-Profit Organization | [Code](https://github.com/Karan9616/NonProfitOrg) | [Demo](https://non-profit-org.vercel.app/) |
+
 
 ## :handshake: Our Contributors
 
