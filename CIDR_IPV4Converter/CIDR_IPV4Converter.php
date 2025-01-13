f5f70ea540f171df07fe6c0196dab915cf5dce12
Matt <185059017+optomist-5@users.noreply.github.com>
2025-01-12 16:30:41 -0800

    Add files via upload

---
+++ CIDR_IPV4Converter/CIDR_IPV4Converter.php
@@ -0,0 +1,13 @@
+
+<div style="text-align:center">
+	<form action="https://www.ipaddressguide.com/cidr" method="post">
+		<p style="background:#fff;border:1px solid #99A8AE;width:180px;padding:5px 5px 5px 5px;font-size:11px;font-family:'Trebuchet MS',Arial,Sans-serif;">
+			<a href="https://www.ipaddressguide.com" target="_blank"><img src="https://www.ipaddressguide.com/images/ipaddressguide.png" alt="CIDR to IPv4 Address Range Utility Tool | IPAddressGuide" border="0" width="120" height="12" /></a><br />
+			<b>CIDR to IPv4 Conversion</b><br /><br />
+			<label>CIDR</label><br />
+			<input type="text" name="cidr" value="" style="border:solid 1px #C0C0C0;font-size:9px;width:110px;" /><br />
+			<input type="submit" value="Calculate" style="width:100px;font-size:10px;margin-top:3px;padding:2px 3px;color:#FFF;background:#8EB50C;border-width:1px;border-style:solid;">
+		</p>
+	</form>
+</div>
+
